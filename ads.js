93ff2dd7ae8ff2545f38b5396ac37ed209136f0d
venusofstar <sumidomikeangelo24@gmail.com>
2026-02-01 19:04:43 +0800

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,48 @@
+<!-- ================= ADS / MONETAG (SAFE LOAD) ================= -->
+
+<!-- Monetag Banner / Pop -->
+<script src="https://quge5.com/88/tag.min.js"
+        data-zone="207250"
+        async
+        data-cfasync="false"></script>
+
+<!-- Monetag Push / Direct -->
+<script>
+(function () {
+  const s = document.createElement("script");
+  s.src = "https://al5sm.com/tag.min.js";
+  s.dataset.zone = "10547491";
+  s.async = true;
+  s.setAttribute("data-cfasync", "false");
+  document.body.appendChild(s);
+})();
+</script>
+
+<!-- Monetag Vignette (fullscreen on navigation) -->
+<script>
+(function () {
+  const s = document.createElement("script");
+  s.src = "https://gizokraijaw.net/vignette.min.js";
+  s.dataset.zone = "10547497";
+  s.async = true;
+  s.setAttribute("data-cfasync", "false");
+  document.body.appendChild(s);
+})();
+</script>
+
+<!-- Monetag Interstitial -->
+<script>
+(function () {
+  const s = document.createElement("script");
+  s.src = "https://nap5k.com/tag.min.js";
+  s.dataset.zone = "10547537";
+  s.async = true;
+  s.setAttribute("data-cfasync", "false");
+  document.body.appendChild(s);
+})();
+</script>
+
+<!-- Monetag Backup -->
+<script src="https://3nbf4.com/act/files/tag.min.js?z=10547542"
+        async
+        data-cfasync="false"></script>
